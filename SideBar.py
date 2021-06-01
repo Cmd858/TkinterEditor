2264b9e3659edefe53270142617f63934a8d3fcb
CmdCoder585 <47717085+CmdCoder585@users.noreply.github.com>
2021-06-01 23:59:25 +0100

    Second commit for cleanup and clarity, added loads of comments

--- SideBar.py
+++ SideBar.py
@@ -1,32 +1,26 @@
-import tkinter as tk
 from tkinter import *
-#from main import widgetType
-
-#wigtype = widgetType
 
 class SideBar:
     def __init__(self, parent):
-        self.parent = parent
-        self.window = parent.window
-        self.winW = self.window['width']
+        self.parent = parent  # get parent class for function references
+        self.window = parent.window  # get parent window for widgets
+        self.winW = self.window['width']  # get dimensions vars
         self.winH = self.window['height']
         print(self.winW, self.winH)
-        self.widgets = []
-        self.sidebar = Label(self.window)
-        self.barwidth = 150
-        self.sidebar.place(x=self.winW - self.barwidth, y=0, width=self.barwidth, height=self.winH)
-        self.sidebar['bg'] = 'grey'
+        self.widgets = []  # widgets list
+        self.sidebar = Label(self.window)  # create sidebar area, should probs use frame but ah well
+        self.barwidth = 150  # width of bar, hardcoded for some reason #TODO: make this reference main
+        self.sidebar.place(x=self.winW - self.barwidth, y=0, width=self.barwidth, height=self.winH)  # place sidebar
+        self.sidebar['bg'] = 'grey'  # set background colour and relief of sidebar
         self.sidebar['relief'] = 'ridge'
 
     def create_widget(self, text, convtype):
-        button = Button(self.sidebar, text=text, command=lambda: self.setType(convtype))
-        button.pack(side='top', anchor='nw')
+        button = Button(self.sidebar, text=text, command=lambda: self.setType(convtype))  # make command button
+        button.pack(side='top', anchor='nw')  # pack the widget to north-west (top-left)
 
     def setType(self, convtype):
-        #global wigtype
-        self.parent.widgetType = convtype
-        #self.wigtype = convtype  # func for setting type from widgets
+        self.parent.widgetType = convtype  # change type of widget being deployed
 
     def populateBar(self):
-        self.create_widget('Label', 'label')
+        self.create_widget('Label', 'label')  # populate the sidebar with function widgets
         self.create_widget('Label2', 'label')
--- main.py
+++ main.py
@@ -2,138 +2,60 @@ import tkinter as tk
 from tkinter import *
 from SideBar import SideBar
 
-def tempsetup():
-
-    x1 = 0
-    x2 = 0
-    y1 = 0
-    y2 = 0
-
-    scrW = 500
-    scrH = 400
-    barW = 150
-
-    type = None
-
-    widgets = []
-    widgetType = None
-
-    window = Tk()
-
-
-def main():
-    global scrH, scrW, widgetType
-    window['width'] = 500
-    window['height'] = 400
-    window.geometry(f'{window["width"]}x{window["height"]}')
-    window.title('Tkinter Editor')
-    window['bg'] = 'black'
-
-    sidebar = SideBar(window)#, widgetType) # fixed to prevent warning
-    sidebar.populateBar()
-    setBinds(window)
-
-    window.mainloop()
-
-
-def setBinds(window):
-    window.bind('<Button-1>', clickDrag)
-    window.bind('<ButtonRelease-1>', clickDrag)
-
-
-def clickDrag(event):
-    global x1, x2, y1, y2, scrH, scrW, barW, widgetType
-    etype = str(event.type)
-    print(event.type)
-    if etype == 'ButtonPress':
-        x1 = event.x
-        y1 = event.y
-    elif etype == 'ButtonRelease':
-        print('makingwidget')
-        x2 = event.x
-        y2 = event.y
-        if 0 < x1 < scrW - barW and 0 < y1 < scrH and \
-                0 < x2 < scrW - barW and 0 < y2 < scrH:
-            placeWidget(widgetType, x1, y1, x2, y2)
-
-def placeWidget(wigtype, x1, y1, x2, y2):
-    global widgets, window
-    wigtype = 'label'
-    if wigtype is None:
-        print(None)
-        return None
-    elif wigtype == 'label':
-        print('label')
-        widgets.append(Label(window))#, x=x1, y=y1, width=x2-x1, height=y2-y1))
-        widgets[-1].pack('top')
-
-#########################################################################
-
-
 class WindowManager:
     def __init__(self):
         self.widgetType = None
         self.x1, self.x2, self.y1, self.y2 = 0, 0, 0, 0  # drag box coordinate references
         self.scrW = 500
-        self.scrH = 400
-        self.barW = 150
-        self.window = Tk()
-        self.window['width'] = 500
-        self.window['height'] = 400
-        self.window.geometry(f'{self.window["width"]}x{self.window["height"]}')
-        self.window.title('Tkinter Editor')
-        self.window['bg'] = 'black'
-        self.widgets = []
+        self.scrH = 400  # might make these init params later for flexibility idk
+        self.barW = 150  # width of sidebar
+        self.window = Tk()  # initiate window
+        self.window['width'] = self.scrW  # sets dimensions to set variables
+        self.window['height'] = self.scrH
+        self.window.geometry(f'{self.window["width"]}x{self.window["height"]}')  # set geometry
+        self.window.title('Tkinter Editor')  # set title
+        self.window['bg'] = 'black'  # set background colour
+        self.widgets = []  # empty list for storing screen widgets
         self.coords = []  # coords of widgets as ((x1, y1), (x2, y2))
 
-        self.sidebar = SideBar(self)
-        self.sidebar.populateBar()
-        self.setBinds(self.window)
+        self.sidebar = SideBar(self)  # initiate sidebar
+        self.sidebar.populateBar()  # populate sidebar
+        self.setBinds(self.window)  # set window keybinds for event callbacks
 
     def run(self):
-        self.window.mainloop()
+        self.window.mainloop()  # run main window event loop
 
     def setBinds(self, window):
-        window.bind('<Button-1>', self.clickDrag)
+        window.bind('<Button-1>', self.clickDrag)  # bind tkinter events to callback functions
         window.bind('<ButtonRelease-1>', self.clickDrag)
 
     def clickDrag(self, event):
         #global x1, x2, y1, y2, scrH, scrW, barW, widgetType
-        etype = str(event.type)
+        etype = str(event.type)  # get event type as string for comparison
         print(event.type)
         if etype == 'ButtonPress':
-            self.x1 = event.x
+            self.x1 = event.x  # grab mousedown positions
             self.y1 = event.y
         elif etype == 'ButtonRelease':
-            print('makingwidget')
-            self.x2 = event.x
+            print('makingwidget')  # stating the obvious
+            self.x2 = event.x  # grab mouseup positions
             self.y2 = event.y
             if 0 < self.x1 < self.scrW - self.barW and 0 < self.y1 < self.scrH and \
                     0 < self.x2 < self.scrW - self.barW and 0 < self.y2 < self.scrH:  # double line bc long
-                self.placeWidget(self.widgetType, self.x1, self.y1, self.x2, self.y2)
+                self.placeWidget(self.widgetType, self.x1, self.y1, self.x2, self.y2)  # calls to place the widget
 
     def placeWidget(self, wigtype, x1, y1, x2, y2):
-        #global widgets, window
-        #wigtype = 'label'
-        if wigtype is None:
-            print(None)
-            return None
+        if wigtype is None:  # no type so do nothing
+            print(None)  # remind you you're an idiot for not picking an option
+            return None  # literally just returns, probs don't need a comment here
         elif wigtype == 'label':
-            #print('label')
-            label_frame = tk.Frame(self.window, width=abs(x2-x1), height=abs(y2-y1))
-            label_frame.pack_propagate(0)
-            self.widgets.append((label_frame, Label(label_frame, text='label')))
-            self.widgets[-1][1].pack() # make widgets with pixel lengths
-            # , x=x1, y=y1, width=x2-x1, height=y2-y1))
-            #Label().place()
-            #print(self.widgets)
+            label_frame = tk.Frame(self.window, width=abs(x2-x1), height=abs(y2-y1))  # create frame with abs dimensions
+            label_frame.pack_propagate(0)  # prevent widgets resizing frame
+            self.widgets.append((label_frame, Label(label_frame, text='label')))  # create references to label and frame
+            self.widgets[-1][1].pack()  # make widgets with pixel lengths instead of chars and lines
+            self.widgets[-1][0].place(x=x1 if x2-x1 >= 0 else x2, y=y1 if y2-y1 >= 0 else y2)  # allow for x1 > x1 etc.
+            print(dict(self.widgets[-1][0])['width'], dict(self.widgets[-1][0])['height'])  # debug print
+            self.coords.append(((x1, y1), (x2, y2)))  # coords for reference later
 
-            self.widgets[-1][0].place(x=x1 if x2-x1 >= 0 else x2, y=y1 if y2-y1 >= 0 else y2)
-            #print(self.widgets[-1].x, self.widgets[-1]['y'], self.widgets[-1]['width'], self.widgets[-1]['height'])
-            print(dict(self.widgets[-1][0])['width'], dict(self.widgets[-1][0])['height'])
-            ## think it defines size in chars and cols
-            self.coords.append(((x1, y1), (x2, y2)))
-
-#main()
-win = WindowManager()
-win.run()
+win = WindowManager()  # initiate class
+win.run()  # run event loop
